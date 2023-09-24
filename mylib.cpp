4fe74669f4e53966976024a9eea97d9386aa3d07
MKrecius33 <144428296+MKrecius33@users.noreply.github.com>
2023-09-24 17:05:07 +0300

    Add files via upload

---
+++ mylib.cpp
@@ -0,0 +1,20 @@
+#include "mylib.h"
+
+void outas() {
+  int a;
+  float b;
+  double c;
+  char simbolis;
+  string eilute;
+  cout<<"Ivesk sveika skaiciu:"<<endl;
+  cin >>a;
+  cout<<"Ivesk realuji skaiciu:"<<endl;
+  cin >>b;
+  cout<<"Ivesk realuji skaiciu:"<<endl;
+  cin >>c;
+  cout<<"Ivesk raide:"<<endl;
+  cin>>simbolis;
+  cout<<"Ivesk zodi:"<<endl;
+  cin>>eilute;
+  cout << a << "; "<<b << "; "<<c <<"; "<<simbolis <<"; "<<eilute<< "\n"<< endl;
+}
---
+++ mylib.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using std::cout;
+using std::cin;
+using std::string;
+using std::endl;
+using std::vector;
+
+
+void outas() ;
