631ed4baea90a5fad34fa0b544647ccd24e38291
MKrecius33 <144428296+MKrecius33@users.noreply.github.com>
2023-09-26 18:23:04 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include "mylib.h"
 
 
-int main() {
+/*int main() {
   float g;
   studentas a;
   cout<<"Iveskite varda: ";
@@ -15,7 +15,7 @@ int main() {
   g = a.paz*0.4+a.egz*0.6;
   cout<<endl<<a.var<<" "<<a.pav<<" Galutinis (Vid.) "<<g<<endl;
 return 0;
-}
+}*/
 
 
 
@@ -52,3 +52,34 @@ return 0;
   printf("%-20s%-20s%-3d%-3d%-3d%-10s%-5.2f","a","b",c,d,e,"Galutinis: ",f);
   return 0;
 }*/
+
+ //bandymas isvesti n studentu
+int main() {
+  string a,b;
+  double x[100];
+  int n;
+  double s=0;
+  float k=0;
+  char c;
+  do{
+    cout<<"Iveskite studento varda: ";
+    cin>>a;
+    cout<<"Iveskite studento pavarde: ";
+    cin>>b;
+    cout<<"Iveskite namu darbu pazymiu skaiciu: \n";
+    cin>>n;
+    cout<<"Namu darbu skaicius yra: "<<n<<endl<<endl;
+    cout<<"Iveskite namu darbu pazymi/ius: \n";
+    for(int i=0; i<n;i++){
+      cout<<i+1<<" -aji pazymi ";
+      cin>>x[i];
+      cout<<"Ivestas "<<i+1<<" pazymis "<<x[i]<<endl;
+      s+=x[i];
+      k=s/n;}
+    cout<<"Ar norite ivesti kita studenta?: t/n\n";
+    cin>>c;
+    } while (c!='n');
+  cout<<endl<<a<<" "<<b<<" Galutinis (Vid.) "<<k<<endl;
+      return 0;}
+  
+    
